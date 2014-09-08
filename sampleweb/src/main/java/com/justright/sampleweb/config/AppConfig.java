7b119720c242b9f19f1cb88a2abe398dfe9d4e98
Torbjørn Nilsen <torbjorn.nilsen@gmail.com>
2014-09-08 22:28:47 +0200

    ContextListener.

--- sampleweb/src/main/java/com/justright/sampleweb/config/AppConfig.java
+++ sampleweb/src/main/java/com/justright/sampleweb/config/AppConfig.java
@@ -1,20 +1,31 @@
 package com.justright.sampleweb.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.justright.config.AbstractAppConfig;
-import com.justright.entrypoint.Entrypoint;
 import com.justright.entrypoint.EntrypointMap;
 import com.justright.entrypoint.ErrorEntrypoint;
 import com.justright.sampleweb.exception.SampleErrorEntrypoint;
 import com.justright.sampleweb.hello.entrypoint.HelloWorld;
 import com.justright.sampleweb.hello.entrypoint.HelloWorldContent;
 import com.justright.sampleweb.login.entrypoint.Login;
+import com.justright.servlet.ContextProcessor;
 import com.justright.servlet.ContextProcessorList;
 
 @Configuration
 public class AppConfig extends AbstractAppConfig{
+	
+	@Override
+	@Bean
+	public ContextProcessorList contextProcessors() {
+		List<ContextProcessor> contextProcessorList = new ArrayList<ContextProcessor>();
+		ContextProcessorList contextProcessors = new ContextProcessorList(contextProcessorList);
+		return contextProcessors;
+	}
 	
 	@Bean
 	public EntrypointMap entrypoints(){
@@ -30,9 +41,4 @@ public class AppConfig extends AbstractAppConfig{
 		return new SampleErrorEntrypoint();
 	}
 
-	@Override
-	public ContextProcessorList contextProcessors() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
